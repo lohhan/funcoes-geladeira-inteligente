398921c563001d4f4cd188b579f207075ac86c57
Lohhan <lohhang05@gmail.com>
2024-09-03 21:47:52 -0300

    basefiles

--- dados.py
+++ dados.py
@@ -1,17 +1,29 @@
+import datetime
 usuario_logado = True
+
+data_hoje = datetime.date.today()
+
+delta = datetime.timedelta(days=-7)
+data_passada = data_hoje + delta
 
 usuarios_moradores = {
    "user1": {
-      "id": 123,
-      "senha": 123456
+      "id_app": 123,
+      "senha": 123456,
+      "email": "teste@gmail.com",
+      "conta": 123456101
    },
    "user2": {
-      "id": 456,
-      "senha": 654321
+      "id_app": 456,
+      "senha": 654321,
+      "email": "teste@gmail.com",
+      "conta": 123456201
    },
    "user3": {
-      "id": 789,
-      "senha": 999111
+      "id_app": 789,
+      "senha": 999111,
+      "email": "teste@gmail.com",
+      "conta": 123456301
    },
 }
 
@@ -19,28 +31,37 @@ produtos = {
     "coca-cola": {
         "preco": 3.50,
         "id": 111,
-        "qtd_disponivel": 10
+        "qtd_disponivel": 10,
+        "data_validade": data_passada
     },
     "heineken": {
         "preco": 7.99,
         "id": 222,
-        "qtd_disponivel": 10
+        "qtd_disponivel": 10,
+        "data_validade": data_hoje
     },
     "pergola": {
         "preco": 25.0,
         "id": 333,
-        "qtd_disponivel": 10
+        "qtd_disponivel": 10,
+        "data_validade": data_hoje
     },
     "toddynho": {
         "preco": 2.70,
         "id": 444,
-        "qtd_disponivel": 10
+        "qtd_disponivel": 10,
+        "data_validade": data_hoje
     },
     "oq": {
         "preco": 16.0,
         "id": 555,
-        "qtd_disponivel": 10
+        "qtd_disponivel": 10,
+        "data_validade": data_hoje
     }
 }
 
 historico_compras = []
+
+ids_gerados = set()
+
+senha_gerada = set()
